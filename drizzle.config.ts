55b0cd0af2a2426190ba12cbadd7665eb66e9c44
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2025-06-04 16:28:36 +0200

    Setup database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,14 @@ import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
+  dialect: "singlestore",
   tablesFilter: ["drive-tutorial_*"],
+  dbCredentials: {
+    host: process.env.SINGLESTORE_HOST!,
+    port: Number(process.env.SINGLESTORE_PORT!),
+    user: process.env.SINGLESTORE_USER!,
+    password: process.env.SINGLESTORE_PASS!,
+    database: process.env.SINGLESTORE_DB_NAME!,
+    ssl: {},
+  },
 } satisfies Config;
