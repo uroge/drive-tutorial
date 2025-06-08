0378eb41d89e8ff9270d7ab1b3bfe81d2218d302
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2025-06-08 11:21:27 +0200

    Cleanup data access pattern

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,31 +1,9 @@
-import { eq } from "drizzle-orm";
 import DriveContents from "~/app/drive-contents";
-import { db } from "~/server/db";
 import {
-  files_table as filesSchema,
-  folders_table as foldersSchema,
-} from "~/server/db/schema";
-
-const getAllParents = async (folderId: number) => {
-  const parents = [];
-  let currentId: number | null = folderId;
-
-  while (currentId !== null) {
-    const folder = await db
-      .selectDistinct()
-      .from(foldersSchema)
-      .where(eq(foldersSchema.id, currentId));
-
-    if (!folder[0]) {
-      throw new Error("Folder not found");
-    }
-
-    parents.unshift(folder[0]);
-    currentId = folder[0]?.parent;
-  }
-
-  return parents;
-};
+  getAllParentsForFolders,
+  getFiles,
+  getFolders,
+} from "~/server/db/queries";
 
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>;
@@ -37,17 +15,9 @@ export default async function GoogleDriveClone(props: {
     return <div>Invalid folder ID</div>;
   }
 
-  const filesPromise = db
-    .select()
-    .from(filesSchema)
-    .where(eq(filesSchema.parent, parsedFolderId));
-
-  const foldersPromise = db
-    .select()
-    .from(foldersSchema)
-    .where(eq(foldersSchema.parent, parsedFolderId));
-
-  const parentsPromise = getAllParents(parsedFolderId);
+  const filesPromise = getFiles(parsedFolderId);
+  const foldersPromise = getFolders(parsedFolderId);
+  const parentsPromise = getAllParentsForFolders(parsedFolderId);
 
   const [files, folders, parents] = await Promise.all([
     filesPromise,
@@ -55,11 +25,5 @@ export default async function GoogleDriveClone(props: {
     parentsPromise,
   ]);
 
-  return (
-    <DriveContents
-      files={files ?? []}
-      folders={folders ?? []}
-      parents={parents}
-    />
-  );
+  return <DriveContents files={files} folders={folders} parents={parents} />;
 }
